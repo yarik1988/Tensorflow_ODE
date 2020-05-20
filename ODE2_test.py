087d1173d8ab5fd57489040539bec729f05782c6
yarik1988 <yaroslav.linder@gmail.com>
2020-05-20 17:10:29 +0300

    update
    update

--- ODE2_test.py
+++ ODE2_test.py
@@ -27,9 +27,9 @@ def train_step(model, optimizer, x):
             y=model(x)
             dy = gg.gradient(y, x)
         ddy=g.gradient(dy, x)
-        residual=ddy-tf.math.square(y)
+        residual=ddy-6*tf.math.square(y)
         loss = tf.math.square(residual)
-        start_loss = (tf.math.square(y[0]-1)+tf.math.square(dy[0]))*N
+        start_loss = (tf.math.square(y[0]-1)+tf.math.square(dy[0]+2))*N
         start_loss=tf.concat([start_loss, tf.zeros(N-1,dtype='float64')], 0)
         total_loss = loss+start_loss
         gradients = g.gradient(total_loss, model.trainable_variables)
@@ -47,28 +47,34 @@ for k in range(5):
     model.add(layers.Dense(50, activation='elu'))
 model.add(layers.Dense(1, use_bias = False))
 
-lr_schedule = keras.optimizers.schedules.ExponentialDecay(0.001,decay_steps=100000,decay_rate=0.99)
+lr_schedule = keras.optimizers.schedules.ExponentialDecay(0.001,decay_steps=100,decay_rate=0.99)
 
 optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 
 bar = tqdm()
 keyboard.on_press_key("q", press_key_exit)
+min_loss=np.Inf
+best_model=tf.keras.models.clone_model(model)
 while True:
     if ready_to_exit:
         break
     grid_tf_run=tf.expand_dims(gx,axis=1)
     loss = train_step(model, optimizer, grid_tf_run)
-    bar.set_description("Loss: {:.5f} {:.5f}".format(*loss))
+    if (sum(loss)<min_loss):
+        min_loss=sum(loss)
+        best_model.set_weights(model.get_weights()) 
+    bar.set_description("Loss: {:.5f} {:.5f} min loss: {:.6f}".format(*loss,min_loss))
     bar.update(1)
 
 #solve by Runge kutta
 def diff(t, z):
-    return [z[1],z[0]*z[0]]
-sol = solve_ivp(diff,[1, T], [1,0],dense_output=True)
+    return [z[1],6*z[0]*z[0]]
+sol = solve_ivp(diff,[1, T], [1,-2],dense_output=True)
 z = sol.sol(gx)
-y = model(gx)
+y = best_model(gx)
 plt.plot(gx,np.squeeze(y))
 plt.plot(gx, z[0].T)
+plt.plot(gx, 1.0/gx**2)
 plt.title('Solving equation $y\'\'=y^2$')
-plt.legend(['Tensorflow','Runge-Kutta'])
+plt.legend(['Tensorflow','Runge-Kutta','Theoretical'])
 plt.show()
